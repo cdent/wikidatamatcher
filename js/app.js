b63a5ba465a0eba0115ee623a25af83bb8a51187
Jonathan Lister <jonathanlister@Jonathan-Listers-MacBook.local>
2009-11-08 18:42:06 +0000

    getting better at fuzzy matching

--- js/app.js
+++ js/app.js
@@ -13,18 +13,8 @@ function addAdvSearchLine() {
 }
 $(document).ready(function() {
 	var url = "http://wiki-data.com/search.json?";
-	var queryVars = {
-		count:0,
-		vars:{}
-	};
+	var queryVars = {};
 	var $container;
-	var noResults = function() {
-		$container = $('#noMatch');
-		$container.find('a').click(function() {
-			$('#searchExplanation').show();
-		});
-		$container.show();
-	};
 	var secondRound;
 	var mergeUnique = function(to,from,by) {
 		var match;
@@ -43,33 +33,56 @@ $(document).ready(function() {
 	};
 	var processJSON = function(records) {
 		if(records.length===0) {
-			if(!secondRound) {
+			// get an ordered list of queryVars to make sequential queries on, along with "q"
+			var vars = [];
+			var notForSearch = [
+				"trading_status",
+				"registered_country",
+				"operational_city",
+				"operational_state",
+				"operational_country"
+			];
+			for(var i in queryVars) {
+				if(i!=="q" && jQuery.inArray(i,notForSearch)===-1) {
+					vars.push(i);
+				}
+			}
+			console.log(vars);
+			if(!secondRound && vars.length>1) {
 				secondRound = true;
 				var count = 0;
 				var secondRoundRecords = [];
-				// get an ordered list of queryVars to make sequential queries on
-				var vars = [];
-				for(var i in queryVars) {
-					vars.push(i);
-				}
 				var v;
 				var str = "";
 				var secondRoundCallback = function(records) {
 					count++;
+					console.log("unmerged:",secondRoundRecords,records);
 					mergeUnique(secondRoundRecords,records,"title");
+					console.log("merged:",secondRoundRecords,records);
+					console.log(count,vars.length);
 					if(count<vars.length) {
 						v = vars[count];
-						str = v+"="+queryVars[v]+"&jsonp_callback=?";
+						str = "q="+queryVars.q+"&"+v+"="+queryVars[v]+"&jsonp_callback=?";
+						console.log('going to get:',url+str);
 						$.getJSON(url+str,secondRoundCallback);
 					} else {
+						console.log('done with second round',secondRoundRecords);
 						processJSON(secondRoundRecords);
 					}
 				};
 				v = vars[0];
-				str = v+"="+queryVars[v]+"&jsonp_callback=?";
-				$.getJSON(url, secondRoundCallback);
+				str = "q="+queryVars.q+"&"+v+"="+queryVars[v]+"&jsonp_callback=?";
+				console.log(url+str);
+				$.getJSON(url+str, secondRoundCallback);
+				$container = $('#noMatchRoundOne');
 			} else {
-				$('noMatches').show();
+				if($container) {
+					$container.hide();
+				}
+				$container = $('#noMatchRoundTwo');
+				$container.find('a').click(function() {
+					$('#searchExplanation').show();
+				});
 			}
 		} else if(records.length===1) {
 			var title = records[0].title;
@@ -126,15 +139,13 @@ $(document).ready(function() {
 				}
 				maxMatches++;
 				for(var i in queryVars) {
-					if(i.match(/adv_\d{1,2}_field/)) {
-						field = queryVars[i].toLowerCase().replace(/ |\(|\)/g,"_");
-						val = queryVars[i.replace('_field', '_value')].toLowerCase();
-						if(val && fields[field]) {
-							if(fields[field].toLowerCase().indexOf(val)!==-1) {
-								matches++;
-							}
-							maxMatches++;
+					field = i;
+					val = queryVars[i];
+					if(fields[field]) {
+						if(fields[field].toLowerCase().indexOf(val)!==-1) {
+							matches++;
 						}
+						maxMatches++;
 					}
 				}
 				perc = Math.floor((matches / maxMatches)*100);
@@ -159,10 +170,21 @@ $(document).ready(function() {
 		$('#results > div').hide();
 		// carry on with this search
 		var str = "";
+		var inputVars = {};
 		$form.find(':input:not(:submit)').each(function() {
-			queryVars[$(this).attr('name')] = $(this).val();
+			inputVars[$(this).attr('name')] = $(this).val();
 		});
-		for(var i in queryVars) {
+		for(var i in inputVars) {
+			if(i==="q") {
+				queryVars.q = inputVars[i];
+			}
+			if(i.match(/^adv_\d{1,2}_field/)) {
+				field = inputVars[i].toLowerCase().replace(/ |\(|\)/g,"_");
+				val = inputVars[i.replace('_field', '_value')].toLowerCase();
+				queryVars[field] = val;
+			}
+		}
+		for(i in queryVars) {
 			str += "&"+i+"="+queryVars[i];
 		}
 		str = str.substring(1);
